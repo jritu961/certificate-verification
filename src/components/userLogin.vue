3a650d53063c7b93524bc9093a6e896b07f8ff00
Ritu Jaiswal <jritu961@gmail.com>
2022-08-31 16:22:14 +0530

    updated userlogin

--- src/components/userLogin.vue
+++ src/components/userLogin.vue
@@ -2,13 +2,13 @@
 <div class="page">
 <NavBar/>
   <section class="vh-100 gradient-custom">
-  <div class="container py-2 h-1">
+  <div class="container py-1 h-1">
     <div class="row d-flex justify-content-center align-items-center h-1">
-      <div class="col-8 col-md-8 col-lg-6 col-xl-5">
+      <div class="col-6 col-md-11 col-lg-6 col-xl-5">
         <div class="card bg-dark text-white" style="border-radius: 1rem;">
           <div class="card-body p-5 text-center">
 
-            <div class="mb-md-5 mt-md-4 pb-5">
+            <div class="mb-md-4 mt-md-4 pb-5">
 
               <h2 class="fw-bold mb-2 text-uppercase">Login</h2>
               <p class="text-white-50 mb-5">Please enter your login and password!</p>
