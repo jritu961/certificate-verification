45ede54e6781c2fcb7d95bc589636542445a44a9
Ritu Jaiswal <jritu961@gmail.com>
2022-09-03 00:04:47 +0530

    register

--- src/components/userRegister.vue
+++ src/components/userRegister.vue
@@ -3,14 +3,14 @@
    <div>
     <NavBar/>
     <section class="h-100 h-custom" style="background-color: #8fc4b7;">
-  <div class="container py-1 h-100">
+  <div class="container py-0 h-90">
     <div class="row d-flex justify-content-center align-items-center h-100">
       <div class="col-lg-8 col-xl-4">
         <div class="card rounded-3">
           <!-- <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-registration/img3.webp"
             class="w-100" style="border-top-left-radius: .3rem; border-top-right-radius: .3rem;"
             alt="Sample photo"> -->
-          <div class="card-body p-4 p-md-5">
+          <div class="card-body p-3 p-md-1">
             <h3 class="mb-4 pb-2 pb-md-0 mb-md-5 px-md-2">Registration Info</h3>
 
             <form class="px-md-2">
@@ -28,15 +28,16 @@
               </div>
                 <div class="col-md-6 mb-4">
                                <div class="col-md-6 mb-4">
-                         
-                   <select class="select" v-model="roles">
+                      <select class="select" v-model="roles">
+              <input type="text" v-model="text" id="form3Example1q" class="form-control" />
+                <label class="form-label" for="form3Example1q" >email</label>
+
                     <option value="1" disabled>Roles</option>
                     <option value="2">student</option>
-                    
-                  </select>
+                   </select>
 
 
-                </div>
+                </div> 
 
                 </div>
                  <input type="Password" v-model="password" id="form3Example1q" class="form-control" />
